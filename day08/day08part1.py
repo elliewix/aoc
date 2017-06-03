d7dabfc38ff9c1f36f61d461fd703e0fc2217e1c
Elizabeth Wickes <elliewix@gmail.com>
2017-06-03 14:36:06 -0500

    day 8 was easy!

--- day08/day08part1.py
+++ day08/day08part1.py
@@ -1,12 +1,12 @@
 import numpy as np
-
+import re
 def change(p):
     if p == 0:
         return 1
     else:
         return 0
 
-screen = np.zeros((3, 7))
+screen = np.zeros((6, 50))
 
 
 def rectAB(screen, a, b):
@@ -31,9 +31,32 @@ def print_screen(screen):
         new += "\n"
     print(new)
 
-rectAB(screen, 3, 2)
-rotate_col(screen, 1, 1)
-rotate_row(screen, 0, 4)
-rotate_col(screen, 1, 1)
-print_screen(screen)
+def read_command(screen, line):
+    if line.startswith("rect"):
+        x, y = re.findall(r'(\d+)x(\d+)', line)[0]
+        rectAB(screen, int(x), int(y))
+    elif line.startswith("rotate column"):
+        col, n = re.findall(r'(\d+) by (\d+)', line)[0]
+        rotate_col(screen, int(col), int(n))
+    elif line.startswith("rotate row"):
+        col, n = re.findall(r'(\d+) by (\d+)', line)[0]
+        rotate_row(screen, int(col), int(n))
+    else:
+        print("I dunno", line)
+
+
+# rectAB(screen, 3, 2)
+# rotate_col(screen, 1, 1)
+# rotate_row(screen, 0, 4)
+# rotate_col(screen, 1, 1)
+# print_screen(screen)
+# print(count_ons(screen))
+
+with open('input.txt', 'rt') as fin:
+    data = [r.strip() for r in fin.readlines()]
+
+for d in data:
+    read_command(screen, d)
+    print_screen(screen)
+
 print(count_ons(screen))
